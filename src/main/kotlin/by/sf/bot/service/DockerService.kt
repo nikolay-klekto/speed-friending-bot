f2433d1aabd9929d7326358b251d8ea2afa5512c
nikolay-klekto <funScrut@gmail.com>
2024-09-12 16:56:34 +0300

    insert restarting app from docker compose [deploy]

--- src/main/kotlin/by/sf/bot/service/DockerService.kt
+++ src/main/kotlin/by/sf/bot/service/DockerService.kt
@@ -8,45 +8,28 @@ import com.github.dockerjava.core.DockerClientBuilder
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient
 import com.github.dockerjava.transport.DockerHttpClient
 import org.springframework.stereotype.Service
+import java.io.File
 import java.time.Duration
 
 @Service
 class DockerService {
 
     fun restartDocker(containerName: String) {
-        val dockerClient = createDockerClient()
+        try {
+            val processBuilder = ProcessBuilder("/home/Nikolay/speedfriendingBot/restart_container.sh", containerName)
+            processBuilder.directory(File("/home/Nikolay/speedfriendingBot"))
+            val process = processBuilder.start()
 
-        // Поиск контейнера по имени
-        val containers: List<Container> = dockerClient.listContainersCmd().exec()
-        val container = containers.find { it.names.any { name -> name == "/$containerName" } }
+            // Ожидание завершения выполнения процесса
+            val exitCode = process.waitFor()
 
-        if (container != null) {
-            val containerId = container.id
-            println("Container found: $containerId. Restarting...")
-
-            // Перезапуск контейнера
-            dockerClient.restartContainerCmd(containerId).exec()
-            println("Container $containerId restarted successfully.")
-        } else {
-            println("Container with name $containerName not found.")
+            if (exitCode == 0) {
+                println("Container $containerName restarted successfully.")
+            } else {
+                println("Failed to restart container $containerName.")
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-    }
-
-    fun createDockerClient(): DockerClient {
-        // Настройка для подключения к Unix-сокету
-        val config = DefaultDockerClientConfig.createDefaultConfigBuilder()
-            .withDockerHost("unix:///var/run/docker.sock")
-            .build()
-
-        val httpClient: DockerHttpClient = ApacheDockerHttpClient.Builder()
-            .dockerHost(config.dockerHost)
-            .maxConnections(100)
-            .connectionTimeout(Duration.ofSeconds(30))
-            .responseTimeout(Duration.ofSeconds(45))
-            .build()
-
-        return DockerClientBuilder.getInstance(config)
-            .withDockerHttpClient(httpClient)
-            .build()
     }
 }
